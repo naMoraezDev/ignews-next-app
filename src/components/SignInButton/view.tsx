21fe7c86c79f35ea8103a247845a6c061f3a043f
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-06 12:24:28 -0300

    feat: implements next-auth

--- src/components/SignInButton/view.tsx
+++ src/components/SignInButton/view.tsx
@@ -1,21 +1,30 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButtonView() {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return !session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
   ) : (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Gabriel Moraes
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   );
 }
