f05508f5dfec67dccceeaa2af71941a7755f6baa
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-08 02:46:01 -0300

    feat: create preogressBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable @next/next/no-css-tags */
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import { SessionProvider as NextAuthProvider } from "next-auth/react";
-
 import "../styles/global.scss";
+import "./nprogress.css";
+import { getProgressBar } from "../utils";
+
+getProgressBar();
 
 export default function App({
   Component,
