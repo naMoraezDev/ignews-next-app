44c85c943f9229fe8e0e5541b106000be1028802
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-13 15:58:28 -0300

    test: Header

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return { asPath: "/" };
+    },
+  };
+});
+
+jest.mock("next-auth/react", () => {
+  return {
+    useSession() {
+      return { data: null, status: "unauthenticated" };
+    },
+  };
+});
+
+describe("Header", () => {
+  it("Should render ActiveLink correctly", () => {
+    render(<Header />);
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Posts")).toBeInTheDocument();
+  });
+});
